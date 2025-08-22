e3893b5ff4af1dc258eb571fd7b28899f6a7fe3b
Paulo Henrique <paulogoodboy264@gmail.com>
2025-08-21 21:08:33 -0300

    feat(add) > adicionado a construção da classe SQLiteDatabaseHelper

--- minhasCompras/helpers/SQLiteDatabaseHelper.cs
+++ minhasCompras/helpers/SQLiteDatabaseHelper.cs
@@ -1,12 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using minhasCompras.models;
+using SQLite;
 
 namespace minhasCompras.helpers
 {
-    internal class SQLiteDatabaseHelper
+    public class SQLiteDatabaseHelper
     {
+        readonly SQLiteAsyncConnection _connection;
+
+        public SQLiteDatabaseHelper(string path)
+        {
+            _connection = new SQLiteAsyncConnection(path);
+            _connection.CreateTableAsync<Produto>().Wait();
+        }
+
+        public Task<int> Insert(Produto produto) 
+        {
+            return _connection.InsertAsync(produto);
+        }
+
+        public Task<List<Produto>> Update(Produto produto)
+        {
+            string sql = "UPDATE Produto SET Descricao = ?, quantidade = ?, Preco = ? WHERE Id = ?";
+
+            return _connection.QueryAsync<Produto>(sql, produto.Descricao, produto.quantidade, produto.Id);
+
+        }
+
+        public Task<int> Delete(int id)
+        {
+            return _connection.Table<Produto>().DeleteAsync(i => i.Id == id);
+        }
+
+        public Task<List<Produto>> GetAllProdutos()
+        {
+            return _connection.Table<Produto>().ToListAsync();
+        }
+
+        public Task<List<Produto>> SearchProduto(string nome)
+        {
+           string sql = "SELECT * FROM Produto WHERE Descricao LIKE ?";
+            return _connection.QueryAsync<Produto>(sql);
+        }
     }
 }
